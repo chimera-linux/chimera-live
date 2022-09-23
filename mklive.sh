6a41927926faae79f000084df83d8404a3a78e2f
Daniel Kolesa <daniel@octaforge.org>
2022-09-23 19:38:22 +0200

    mklive.sh: aarch64 efi should be bootaa64.efi, not boota64.efi

--- mklive.sh
+++ mklive.sh
@@ -362,7 +362,7 @@ generate_grub_efi() {
 case "${APK_ARCH}" in
     ppc*) generate_grub_ppc;;
     x86*) generate_grub_x86;;
-    aarch64*) generate_grub_efi arm64 a64;;
+    aarch64*) generate_grub_efi arm64 aa64;;
     riscv64*) generate_grub_efi riscv64 rv64;;
 esac
 
